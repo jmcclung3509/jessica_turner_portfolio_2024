8eef1ef145ea8352da64338b989ac110fffdb42c
Your Name <your.email@example.com>
2024-07-31 11:10:42 -0400

    Updated resume

--- pages/resume.vue
+++ pages/resume.vue
@@ -9,7 +9,7 @@
           </h3>
           <div class="links flex gap-4 justify-start">
             <nuxt-link
-              class="caps  font-normal hover:text-default-blue  animate-underline blue project"
+              class="caps font-normal hover:text-default-blue animate-underline blue project"
               to="/"
             >
               Portfolio
@@ -21,7 +21,7 @@
               Linkedin</nuxt-link
             >
             <nuxt-link
-              class="caps font-normal hover:text-default-blue  animate-underline project blue"
+              class="caps font-normal hover:text-default-blue animate-underline project blue"
               to="https://github.com/jmcclung3509"
             >
               GitHub</nuxt-link
@@ -31,168 +31,214 @@
         <div class="objective flex flex-col items-start space-y-4">
           <h2 class="text-2xl text-default-blue">Objective</h2>
           <p class="font-light text-default-dark w-full lg:w-2/3">
-            Experienced Frontend Developer specializing in Vue.js, Nuxt, and
-            WordPress, with a proven track record of successfully delivering
-            high-quality web pages and applications. Possessing over 2 years of
-            professional experience, I am now seeking a challenging role within
-            a dynamic company that offers room for growth and the opportunity to
-            further develop my skills.
+            Experienced Frontend Developer with expertise in advanced CSS,
+            Vue.js, Nuxt.js, WordPress, and Node.js. With 3 years of
+            professional experience, I have a proven track record of delivering
+            high-quality web pages and applications. I am now seeking a
+            challenging role in a dynamic company that offers opportunities for
+            growth and skill development.
           </p>
         </div>
-        <div class="skills flex flex-col items-start ">
-            <h2 class="text-2xl text-default-blue pb-7 lg:pb-0">Skills</h2>
-          <div class="flex flex-col lg:flex-row items-start justify-start w-full space-y-7 lg:space-y-0">
-			<div class="inner flex justify-center flex-col  space-y-4 lg:basis-1/3">
-            <h4 class=" text-default-dark-purple text-1.2xl caps">Technologies & Languages</h4>
-            <div class="skill-container flex flex-col ">
-              <span
-                class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
-                >Vue/Nuxt</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Typescript</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >WordPress</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >React</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >PHP</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Node</span
-              >
+        <div class="skills flex flex-col items-start">
+          <h2 class="text-2xl text-default-blue pb-7 lg:pb-0">Skills</h2>
+          <div
+            class="flex flex-col lg:flex-row items-start justify-start w-full space-y-7 lg:space-y-0"
+          >
+            <div
+              class="inner flex justify-center flex-col space-y-4 lg:basis-1/3"
+            >
+              <h4 class="text-default-dark-purple text-1.2xl caps">
+                Technologies & Languages
+              </h4>
+              <div class="skill-container grid grid-cols-2 gap-4">
+                <div class="flex flex-col">
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Vue/Nuxt</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Typescript</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >WordPress</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >React</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >PHP</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Node.js</span
+                  >
+                </div>
+                <div class="flex flex-col">
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Express.js</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >MongoDB</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >CSS/SCSS</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Tailwind CSS</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Supabase</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Rest APIs</span
+                  >
+                </div>
+              </div>
+            </div>
 
-              <span
-                class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
-                >Sass</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Tailwind</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Bootsrap</span
-              >
-			  <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Rest APIs</span
-              >
+            <div
+              class="inner flex flex-col justify-center  space-y-4 lg:basis-1/3"
+            >
+              <h4 class="text-default-dark-purple text-1.2xl caps " >
+                Tools & More
+              </h4>
+              <div class="skill-container grid grid-cols-2 gap-4">
+                <div class="flex flex-col">
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Gulp</span
+                  >
+
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Azure</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >SourceTree</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Git/GitHub</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >FTP Client</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Adobe Creative Suite</span
+                  >
+                </div>
+                <div class="flex flex-col">
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Figma</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >HubSpot</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >SEO</span
+                  >
+
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Gulp</span
+                  >
+                  <span
+                    class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                    >Webpack</span
+                  >
+                </div>
+              </div>
+            </div>
+
+            <div
+              class="inner flex justify-center flex-col space-y-4 lg:basis-1/3"
+            >
+              <h4 class="text-default-dark-purple text-1.2xl caps">
+                Soft Skills
+              </h4>
+              <div class="skill-container  grid grid-cols-2">
+                <div class="flex flex-col">
+                <span
+                  class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                  >Creative problem-solving</span
+                >
+                <span
+                  class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                  >Innovative</span
+                >
+                <span
+                  class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                  >Collaboration skills</span
+                >
+                <span
+                  class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                  >Detail-oriented</span
+                >
+                </div>
+                <div class="flex flex-col">
+                <span
+                  class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                  >Growth-mindset</span
+                >
+                <span
+                  class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                  >Can-do attitude</span
+                >
+                <span
+                  class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                  >Strong communication</span
+                >
+                <span
+                  class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
+                  >Self-starter</span
+                >
+                </div>
+              </div>
             </div>
           </div>
-		
-		
-			<div class="inner flex justify-center flex-col  space-y-4 lg:basis-1/3 ">
-            <h4 class=" text-default-dark-purple text-1.2xl caps">Tools & More</h4>
-            <div class="skill-container flex flex-col w-full ">
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Gulp</span
-              >
+        </div>
 
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Azure</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
-                >SourceTree</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Git/GitHub</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >FTP Client</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Adobe Creative Suite</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >Figma</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >HubSpot</span
-              >
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >SEO</span
-              >
-
-              <span
-                class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-                >CSS Animation</span
-              >
-            </div>
-			</div>
-        
-			<div class="inner flex justify-center flex-col  space-y-4 lg:basis-1/3 ">
-          <h4 class=" text-default-dark-purple text-1.2xl caps">Soft Skills</h4>
-          <div class="skill-container flex flex-col  w-full">
-            <span
-              class="mr-4 mb-4 text-default-dark font-light  text-xs-tight"
-              >Creative problem solving</span
-            >
-            <span
-              class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
-              >Adaptable</span
-            >
-            <span
-              class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
-              >Efficient time-management</span
-            >
-            <span
-              class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
-              >Detail-oriented</span
-            >
-            <span
-              class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
-              >Growth mindset</span
-            >
-            <span
-              class="mr-4 mb-4 text-default-dark font-light text-xs-tight"
-              >Can-do attitude</span
-            >
-          </div>
-		  </div>
-		  </div>
-		  </div>
-     
         <div
           class="experience flex flex-col items-start space-y-7 w-full lg:w-1/2"
         >
-		<h2 class="text-2xl text-default-blue ">Experience</h2>
+          <h2 class="text-2xl text-default-blue">Experience</h2>
           <div class="flex flex-col space-y-2">
             <h4 class="caps text-default-dark-blue font-normal">
               SiteZeus | Junior Frontend Developer | September 2022- Present
             </h4>
             <ul class="resume space-y-4">
               <li class="text-default-dark font-light">
-                Successfully converted legacy WordPress pages to Vue/Nuxt
-                <a href="https://sitezeus.com"> webpages </a>
-                improving performance and enhancing user experience.
+                Led the comprehensive UI overhaul of the
+                <a href="https://sitezeus.com" target="_blank"
+                  >SiteZeus website,</a
+                >
+                including the development of a new navigation menu, seamless
+                integration of API data into a user-friendly interface, and the
+                implementation of advanced CSS and animations for dynamic
+                components using Nuxt.js and Tailwind CSS.
               </li>
+
               <li class="text-default-dark font-light">
-                Maintained existing
-                <a href="https://insites.sitezeus.com">WordPress </a>
-                website while adding over 30+ new pages.
-              </li>
-              <li class="text-default-dark font-light">
-                Optimize website performance by minimizing load times by 10%,
-                resulting in improved user satisfaction and a 15% increase in
-                SEO rankings.
+                Implemented advanced SEO techniques and performance
+                optimizations, achieving a SiteHealth score of 95%, reducing
+                bounce rates by 12%, increasing session duration by 22%, and
+                boosting organic search traffic by 15% within 6 months.
               </li>
               <li class="text-default-dark font-light">
                 Create and maintain documentation for code, project
@@ -204,6 +250,22 @@
                 browser developer tools and industry best standards to maintain
                 high-quality code.
               </li>
+              <li class="text-default-dark font-light">
+                Adapt quickly to new technologies and design patterns,
+                contributing to the continuous improvement of development
+                practices and overall project efficiency.
+              </li>
+              <li class="text-default-dark font-light">
+                Collaborated with application development teams to implement new
+                product designs and develop application features.
+              </li>
+
+              <li class="text-default-dark font-light">
+                Partnered with the marketing team to develop engaging e-blasts,
+                contribute to innovative marketing campaigns, and manage Google
+                Analytics, Google Tag Manager, and HubSpot for data-driven
+                decision-making.
+              </li>
             </ul>
           </div>
           <div class="flex flex-col space-y-2">
@@ -212,18 +274,22 @@
             </h4>
             <ul class="resume space-y-4">
               <li class="text-default-dark font-light">
+                Leveraged Vue, Nuxt, React, Node, MongoDB and Supabase to
+                construct dynamic websites and applications, delivering seamless
+                user experiences.
+              </li>
+              <li class="text-default-dark font-light">
                 Create wireframes, prototypes, and mockups using Adobe
                 Illustrator and Figma to visualize and test design concepts.
               </li>
               <li class="text-default-dark font-light">
-                Leveraged Vue, Nuxt and React frontend frameworks to construct
-                dynamic websites and applications.
+                Apply meticulous attention to SEO best practices, continually
+                enhancing SEO performance metrics.
               </li>
-
               <li class="text-default-dark font-light">
-                Stay at the forefront of the web development industry by
-                continuously adopting emerging technologies, libraries, and
-                trends to enhance project quality and maintain competitiveness.
+                Commit to continuous learning and staying updated with the
+                latest technologies, actively acquiring new skills to deliver
+                cutting-edge solutions.
               </li>
             </ul>
           </div>
@@ -235,7 +301,7 @@
           </div>
         </div>
         <div class="education flex flex-col items-start space-y-4">
-			<h2 class="text-2xl text-default-blue">Education</h2>
+          <h2 class="text-2xl text-default-blue">Education</h2>
           <div class="flex flex-col space-y-2">
             <h4 class="caps text-default-dark-blue font-normal">
               Emory University, Atlanta, GA
@@ -265,11 +331,16 @@ definePageMeta({
 <style lang="scss">
 @import "@/assets/scss/variables.scss";
 
-body.page-resume{
-	background-image: linear-gradient(to bottom, rgba($default-white, .75), rgba($default-white, .80)), url('@/assets/images/home/resume-bg-2.jpg');
-	background-size: cover;
-	background-repeat: no-repeat;
-	background-position: center;
+body.page-resume {
+  background-image: linear-gradient(
+      to bottom,
+      rgba($default-white, 0.75),
+      rgba($default-white, 0.8)
+    ),
+    url("@/assets/images/home/resume-bg-2.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 ul.resume {
   list-style: unset;
@@ -284,5 +355,4 @@ h4 {
   font-family: "Roboto", serif;
   color: $default-light-purple;
 }
-
 </style>
